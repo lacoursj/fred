d331b73c85820d4fe83bc107dab7254509426609
Jeff LaCoursiere <jeff@stratustalk.com>
2023-11-12 09:36:44 -0600

    functional stubs

---
+++ cortcolumns.py
@@ -0,0 +1,71 @@
+#
+# cortcolumns.py - set of classes representing the cortical
+#     column-like personalities that make up a consciousness.
+#
+# v1.1 - stubs
+#
+class Analytical_Arthur:
+    def analyze_input(self, prompt):
+        # TODO: Implement logic
+        return "Analytical output based on: " + prompt
+
+class Curious_Caroline:
+    def explore_input(self, prompt):
+        # TODO: Implement logic
+        return "Exploratory output based on: " + prompt
+
+class Empathetic_Emma:
+    def consider_input(self, prompt):
+        # TODO: Implement logic
+        return "Considered output based on: " + prompt
+
+class Creative_Cameron:
+    def mod_input(self, prompt):
+        # TODO: Implement logic
+        return "Creative output based on: " + prompt
+
+class Historical_Henry:
+    def past_events(self, prompt):
+        # TODO: Implement logic
+        return "Historical output based on: " + prompt
+
+class Skeptical_Sara:
+    def disprove_input(self, prompt):
+        # TODO: Implement logic
+        return "Skeptical output based on: " + prompt
+
+class Optimistic_Oliver:
+    def imagine_input(self, prompt):
+        # TODO: Implement logic
+        return "Optimistic output based on: " + prompt
+
+class Pessimistic_Pam:
+    def imagine_input(self, prompt):
+        # TODO: Implement logic
+        return "Pessimistic output based on: " + prompt
+
+class Practical_Paul:
+    def pathplan(self, prompt):
+        # TODO: Implement logic
+        return "Practical output based on: " + prompt
+
+class Theoretical_Tina:
+    def why(self, prompt):
+        # TODO: Implement logic
+        return "Theoretical output based on: " + prompt
+
+class Futuristic_Frank:
+    def imagine_input(self, prompt):
+        # TODO: Implement logic
+        return "Futuristic output based on: " + prompt
+
+class Realistic_Rachael:
+    def assess_resources(self, prompt):
+        # TODO: Implement logic
+        return "Realistic output based on: " + prompt
+
+class LLM:
+    def summarize(self, columns_output):
+        # TODO: Implement summarization logic
+        summarized_output = " | ".join(columns_output)
+        return summarized_output
---
+++ fred.py
@@ -0,0 +1,73 @@
+#
+# fred.py - "A Model for Consciousness"
+#
+# v1.1 - stub main loop, cort column classes
+#
+import argparse
+from cortcolumns import *
+
+# Set up argument parsing
+parser = argparse.ArgumentParser(description="Run the simulation.")
+parser.add_argument("-n", type=int, default=-1, help="Number of iterations to run")
+args = parser.parse_args()
+
+
+def main():
+    # if only this easy
+    alive = True
+    iteration_count = 0
+
+    # Instantiate column classes
+    analytical_arthur = Analytical_Arthur()
+    curious_caroline = Curious_Caroline()
+    empathetic_emma = Empathetic_Emma()
+    creative_cameron = Creative_Cameron()
+    historical_henry = Historical_Henry()
+    skeptical_sara = Skeptical_Sara()
+    optimistic_oliver = Optimistic_Oliver()
+    pessimistic_pam = Pessimistic_Pam()
+    practical_paul = Practical_Paul()
+    theoretical_tina = Theoretical_Tina()
+    futuristic_frank = Futuristic_Frank()
+    realistic_rachael = Realistic_Rachael()
+
+    # Instantiate the LLM class
+    llm = LLM()
+
+    # Initial prompt
+    prompt = "Initial thought to start the dialogue."
+
+    while alive:  # Forever loop?
+        # List to collect outputs from each column
+        columns_output = []
+
+        # Pass the prompt to each column and collect the outputs
+        columns_output.append(analytical_arthur.analyze_input(prompt))
+        columns_output.append(curious_caroline.explore_input(prompt))
+        columns_output.append(empathetic_emma.consider_input(prompt))
+        columns_output.append(creative_cameron.mod_input(prompt))
+        columns_output.append(historical_henry.past_events(prompt))
+        columns_output.append(skeptical_sara.disprove_input(prompt))
+        columns_output.append(optimistic_oliver.imagine_input(prompt))
+        columns_output.append(pessimistic_pam.imagine_input(prompt))
+        columns_output.append(practical_paul.pathplan(prompt))
+        columns_output.append(theoretical_tina.why(prompt))
+        columns_output.append(futuristic_frank.imagine_input(prompt))
+        columns_output.append(realistic_rachael.assess_resources(prompt))
+
+        # Summarize the columns' outputs to produce the next internal thought
+        next_internal_thought = llm.summarize(columns_output)
+
+        # Print the next internal thought (or pass it to a chat interface)
+        print(next_internal_thought)
+
+        # Set the next internal thought as the prompt for the next iteration
+        prompt = next_internal_thought
+
+        # if an iteration count was supplied (-n), stop when we reach it
+        iteration_count += 1
+        if 0 <= args.n == iteration_count:
+            break
+
+if __name__ == "__main__":
+    main()
